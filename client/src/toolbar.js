de0ed7b8437e154601b8b22ffc0d964ffea0ab97
MauroDavid512 <mauroalosactormza@gmail.com>
2024-01-29 22:06:58 -0300

    inicio bambalinas

---
+++ client/src/toolbar.js
@@ -0,0 +1,14 @@
+const toolbar = [
+    ['bold', 'italic', 'underline', 'strike'],
+    ['blockquote', 'code-block'],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    [{ align: [] }],
+    [{size:[ false, 'large', 'huge']}],
+    [{header: [1,2,3,4,5,6,false]}],
+    ['link', 'image', 'video'],
+    [{indent:'-1'},{indent:'+1'}],
+    [{color:[]}, {background: []}],
+    ["clean"]
+]
+
+export default toolbar
